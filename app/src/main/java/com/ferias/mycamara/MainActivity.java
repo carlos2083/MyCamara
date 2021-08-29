12fd512057275a7c535ff2ff12519a7eebad144d
Carlos Saez <csa20190583@gmail.com>
2021-08-29 16:23:13 -0300

    Initial commit

---
+++ app/src/main/java/com/ferias/mycamara/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ferias.mycamara;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
